3cea0a6f3b2d9a4179cfc7969ab749fa9eee0355
malonge <malonge11@gmail.com>
2018-03-20 12:03:58 -0400

    added option to turn off sv calling

--- ragoo.py
+++ ragoo.py
@@ -277,9 +277,10 @@ if __name__ == "__main__":
     parser.add_argument("-l", metavar="10000", type=int, default=10000, help=argparse.SUPPRESS)
     parser.add_argument("-r", metavar="100000", type=int, default=100000, help=argparse.SUPPRESS)
     parser.add_argument("-c", metavar="1000000", type=int, default=1000000, help=argparse.SUPPRESS)
-    parser.add_argument("-d", metavar="20000000", type=int, default=20000000, help=argparse.SUPPRESS)
+    parser.add_argument("-d", metavar="20000000", type=int, default=20000000, help=argparse.SUPPRESS    )
     parser.add_argument("-t", metavar="3", type=int, default=3, help="Number of threads when running minimap.")
     parser.add_argument("-g", metavar="100", type=int, default=100, help="Gap size for padding in pseudomolecules.")
+    parser.add_argument("-s", action='store_true', default=True, help="Call Structural Variants")
 
     # Get the command line arguments
     args = parser.parse_args()
@@ -297,6 +298,7 @@ if __name__ == "__main__":
     intra_wrt_ctg_min = args.c
     t = args.t
     g = args.g
+    call_svs = args.s
 
     current_path = os.getcwd()
     output_path = current_path + '/ragoo_output'
@@ -436,10 +438,11 @@ if __name__ == "__main__":
     log('-- Creating Pseudomolecules')
     create_pseudomolecules(contigs_file, all_unique_contigs, g)
 
-    log('-- Aligning pseudomolecules to reference')
-    align_pms(minimap_path, t, reference_file)
+    if call_svs:
+        log('-- Aligning pseudomolecules to reference')
+        align_pms(minimap_path, t, reference_file)
 
-    log('-- Getting structural variants')
-    get_SVs()
+        log('-- Getting structural variants')
+        get_SVs()
 
     log('-- goodbye')
