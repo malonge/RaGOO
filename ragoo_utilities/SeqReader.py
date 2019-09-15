394154ba6b57ab05073f15673a1ef3a5ff9507c3
malonge <malonge11@gmail.com>
2019-09-14 23:18:03 -0400

    added utility to read gzipped fasta files

--- ragoo_utilities/SeqReader.py
+++ ragoo_utilities/SeqReader.py
@@ -1,3 +1,6 @@
+import gzip
+
+
 class SeqReader:
 
     def __init__(self, in_file):
@@ -40,10 +43,34 @@ class SeqReader:
                     sequence += ''.join(line.rstrip().split())
         yield header, sequence
 
-    def get_seq(self, in_header):
-        in_header = in_header.replace('>', '')
-        all_seqs = dict()
-        for header, seq in self.parse_fasta():
-            all_seqs[header.replace('>', '')] = seq.rstrip()
+    def parse_gzip_fasta(self):
+        """
+        Generator yielding header and sequence, for each sequence
+        in the fasta file sent to the class. For gzipped fasta files.
+        """
+        with gzip.open(self.in_file) as fasta_file:
+            sequence = ''
+            # Find first header.
+            line = fasta_file.readline().decode('utf-8')
+            while not line.startswith('>'):
+                line = fasta_file.readline().decode('utf-8')
+                if not line:
+                    error = """ This file provided is not in proper fasta format.
+                            In addition to the usual fasta conventions, be sure that there are
+                            no blank lines in the file.
+                            """
+                    raise RuntimeError(error)
+            header = line.rstrip()
 
-        return in_header, all_seqs[in_header]
+            # Get sequence associated with that header.
+            for line in fasta_file:
+                line = line.decode('utf-8')
+                if line.startswith('>'):
+                    # Once the sequence is over, (next header begins),
+                    # yield initial header and sequence.
+                    yield header, sequence
+                    header = line.rstrip()
+                    sequence = ''
+                else:
+                    sequence += ''.join(line.rstrip().split())
+        yield header, sequence
